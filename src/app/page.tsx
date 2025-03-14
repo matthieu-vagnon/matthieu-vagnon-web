fa812833b83f09e0d8e1e719cdf8f29e9f748161
Matthieu Vagnon <main.mvagnon@icloud.com>
2025-03-14 12:12:02 +0900

    Adding dynamic landing page background.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,11 @@ import LandingTitle from '@/components/LandingTitle'
 
 export default function Home() {
   return (
-    <div className='flex flex-col items-center py-16 px-8 min-h-screen justify-center'>
+    <div className='flex flex-col'>
       <LandingTitle />
+      <div className='flex flex-col items-center py-16 px-8 min-h-screen justify-center'>
+        <h1>Hello, I&apos;m Matthieu Vagnon</h1>
+      </div>
     </div>
   )
 }
--- src/components/LandingTitle.tsx
+++ src/components/LandingTitle.tsx
@@ -4,42 +4,48 @@ import { BlurFade } from '@/components/ui/blur-fade'
 import { Card, CardContent } from '@/components/ui/card'
 import { TextRotate } from '@/components/ui/text-rotate'
 import { LayoutGroup, motion } from 'motion/react'
+import { Particles } from './ui/particles'
 
 export default function LandingTitle() {
   return (
     <LayoutGroup>
-      <motion.div layout>
-        <BlurFade className='flex'>
-          <Card variant='dots'>
-            <CardContent className='flex flex-col gap-1 md:gap-3 whitespace-pre items-center font-sans text-xl sm:text-3xl md:text-4xl translate-y-[6px]'>
-              <BlurFade delay={0.1} className='flex'>
-                <motion.span layout transition={{ type: 'spring', damping: 30, stiffness: 400 }}>
-                  Hello, I&apos;m <span className='font-bold'>Matthieu Vagnon</span>,
-                </motion.span>
-              </BlurFade>
-              <BlurFade delay={0.2} className='flex'>
-                <span className='flex items-center mt-1 md:mt-2'>
-                  <motion.span layout transition={{ type: 'spring', damping: 30, stiffness: 400 }}>
-                    Freelance{' '}
-                  </motion.span>
-                  <TextRotate
-                    texts={['Front-End Engineer', 'Digital Designer', 'Website Creator']}
-                    mainClassName='text-white px-2 sm:px-2 md:px-3 bg-[#007fff] overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg'
-                    staggerFrom={'last'}
-                    initial={{ y: '100%' }}
-                    animate={{ y: 0 }}
-                    exit={{ y: '-120%' }}
-                    staggerDuration={0.025}
-                    splitLevelClassName='overflow-hidden pb-0.5 sm:pb-1 md:pb-1'
-                    transition={{ type: 'spring', damping: 30, stiffness: 400 }}
-                    rotationInterval={3000}
-                  />
-                </span>
-              </BlurFade>
-            </CardContent>
-          </Card>
-        </BlurFade>
-      </motion.div>
+      <div className='relative h-screen w-screen p-4 flex justify-center items-center overflow-hidden'>
+        <motion.div layout>
+          <BlurFade className='flex'>
+            <motion.div layout transition={{ type: 'spring', damping: 30, stiffness: 400 }}>
+              <Card variant='dots' className='h-fit w-fit bg-white'>
+                <CardContent className='flex flex-col gap-1 md:gap-3 whitespace-pre items-center font-sans text-xl sm:text-3xl md:text-4xl translate-y-[6px]'>
+                  <BlurFade delay={0.1} className='flex'>
+                    <motion.span layout transition={{ type: 'spring', damping: 30, stiffness: 400 }}>
+                      Hello, I&apos;m <span className='font-bold'>Matthieu Vagnon</span>,
+                    </motion.span>
+                  </BlurFade>
+                  <BlurFade delay={0.2} className='flex'>
+                    <span className='flex items-center mt-1 md:mt-2'>
+                      <motion.span layout transition={{ type: 'spring', damping: 30, stiffness: 400 }}>
+                        Freelance{' '}
+                      </motion.span>
+                      <TextRotate
+                        texts={['Front-End Engineer', 'Digital Designer', 'Website Creator']}
+                        mainClassName='text-white px-2 sm:px-2 md:px-3 bg-[#007fff] overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg'
+                        staggerFrom={'last'}
+                        initial={{ y: '100%' }}
+                        animate={{ y: 0 }}
+                        exit={{ y: '-120%' }}
+                        staggerDuration={0.025}
+                        splitLevelClassName='overflow-hidden pb-0.5 sm:pb-1 md:pb-1'
+                        transition={{ type: 'spring', damping: 30, stiffness: 400 }}
+                        rotationInterval={3000}
+                      />
+                    </span>
+                  </BlurFade>
+                </CardContent>
+              </Card>
+            </motion.div>
+          </BlurFade>
+        </motion.div>
+        <Particles className='absolute inset-0 -z-1' quantity={100} ease={80} color='black' refresh />
+      </div>
     </LayoutGroup>
   )
 }
