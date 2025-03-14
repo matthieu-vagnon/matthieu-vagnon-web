edddac490f70f832803dfd3d15ee8af8e328d47c
Matthieu Vagnon <main.mvagnon@icloud.com>
2025-03-14 12:14:29 +0900

    Removing useless content.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,5 @@
 import LandingTitle from '@/components/LandingTitle'
 
 export default function Home() {
-  return (
-    <div className='flex flex-col'>
-      <LandingTitle />
-      <div className='flex flex-col items-center py-16 px-8 min-h-screen justify-center'>
-        <h1>Hello, I&apos;m Matthieu Vagnon</h1>
-      </div>
-    </div>
-  )
+  return <LandingTitle />
 }
