e7bc861637ad257989b1c4fbe22fa7675948513f
Matthieu Vagnon <main.mvagnon@icloud.com>
2025-03-14 13:02:09 +0900

    Enhancements on logo responsiveness.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -58,8 +58,11 @@ export default function RootLayout({
       <body
         className={`${signikaNegative.variable} antialiased max-w-[3840px] mx-auto relative min-h-screen px-4 sm:px-6 md:px-8 overflow-x-hidden`}
       >
-        <Link href='/' className='absolute top-4 sm:top-6 md:top-8 left-4 sm:left-6 md:left-8'>
-          <svg width='50' height='50' viewBox='0 0 800 800' fill='none' xmlns='http://www.w3.org/2000/svg'>
+        <Link
+          href='/'
+          className='absolute top-4 sm:top-6 md:top-8 left-4 sm:left-6 md:left-8 h-8 w-8 sm:h-10 sm:w-10 md:h-12 md:w-12'
+        >
+          <svg viewBox='0 0 800 800' fill='none' xmlns='http://www.w3.org/2000/svg'>
             <rect width='800' height='800' fill='#001428' />
             <rect y='650' width='800' height='150' fill='#007FFF' />
           </svg>
