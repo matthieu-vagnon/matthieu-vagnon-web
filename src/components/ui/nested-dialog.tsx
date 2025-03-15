aeb3a20329a6b475e71dad1d41a2993aa5304efb
Matthieu Vagnon <main.mvagnon@icloud.com>
2025-03-15 16:25:59 +0900

    UI minor.

--- src/components/ui/nested-dialog.tsx
+++ src/components/ui/nested-dialog.tsx
@@ -62,7 +62,7 @@ const DialogContent = React.forwardRef<
         ref={ref}
         className={cn(
           'fixed left-1/2 top-1/2 z-98 grid w-full max-w-lg -translate-x-1/2 -translate-y-1/2 gap-4 rounded-lg border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-bottom-1/2 data-[state=open]:slide-in-from-bottom-1/2 sm:rounded-lg',
-          context.innerOpen && 'translate-y-[-60%] translate-x-[-60%] scale-[0.97]',
+          context.innerOpen && 'translate-y-[-60%] translate-x-[-55%] scale-[0.97]',
           className
         )}
         {...props}
