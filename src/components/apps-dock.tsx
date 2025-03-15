b7727af9bd78a6909caa32dd15307bc50f306ef0
Matthieu Vagnon <main.mvagnon@icloud.com>
2025-03-15 20:28:52 +0900

    Bugfixing dock changing state in modal.

--- src/components/apps-dock.tsx
+++ src/components/apps-dock.tsx
@@ -3,7 +3,7 @@
 import { useDockStatus } from '@/hooks/use-dock-status'
 import { HomeIcon, MessageCircle } from 'lucide-react'
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 import { ContactModal } from './contact-modal'
 import { Dock, DockIcon, DockItem, DockLabel } from './ui/dock'
 import { Dialog, DialogTrigger } from './ui/nested-dialog'
@@ -48,42 +48,41 @@ const APPS: App[] = [
   // }
 ]
 
-function AppWrapper({ app, children }: { app: App; children: React.ReactNode }) {
-  if (app.url) {
-    return <Link href={app.url}>{children}</Link>
-  }
-
-  if (app.modal) {
-    return (
-      <Dialog>
-        <DialogTrigger>{children}</DialogTrigger>
-        {app.modal}
-      </Dialog>
-    )
-  }
-
-  return children
+function DockElement({ app, onClick }: { app: App; onClick?: () => void }) {
+  return (
+    <Link href={app.url ?? '#'} onClick={onClick}>
+      <DockItem className='aspect-square rounded-full bg-gray-200 dark:bg-neutral-800 cursor-pointer'>
+        <DockLabel>{app.title}</DockLabel>
+        <DockIcon>{app.icon}</DockIcon>
+      </DockItem>
+    </Link>
+  )
 }
 
 export default function AppsDock() {
   const { isDockOpen } = useDockStatus()
+  const [activeModal, setActiveModal] = useState<number | undefined>(undefined)
 
   return (
-    <div
-      className={`fixed bottom-2 left-1/2 w-full -translate-x-1/2 z-100 ${
-        true ? 'translate-y-0' : 'translate-y-full'
-      } transition-transform duration-300`}
-    >
-      <Dock className='items-end pb-3'>
-        {APPS.map((app, idx) => (
-          <AppWrapper key={idx} app={app}>
-            <DockItem className='aspect-square rounded-full bg-gray-200 dark:bg-neutral-800 cursor-pointer'>
-              <DockLabel>{app.title}</DockLabel>
-              <DockIcon>{app.icon}</DockIcon>
-            </DockItem>
-          </AppWrapper>
-        ))}
-      </Dock>
-    </div>
+    <Dialog>
+      <div
+        className={`fixed bottom-2 left-1/2 w-full -translate-x-1/2 z-100 ${
+          isDockOpen ? 'translate-y-0' : 'translate-y-full'
+        } transition-transform duration-300`}
+      >
+        <Dock className='items-end pb-3'>
+          {APPS.map((app, idx) =>
+            app.modal ? (
+              <DialogTrigger key={idx} asChild>
+                <DockElement app={app} onClick={() => setActiveModal(idx)} />
+              </DialogTrigger>
+            ) : (
+              <DockElement key={idx} app={app} />
+            )
+          )}
+        </Dock>
+      </div>
+      {activeModal !== undefined && APPS[activeModal].modal}
+    </Dialog>
   )
 }
