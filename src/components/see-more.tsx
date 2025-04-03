588a792211e7e61e96b7f922f4b0a9888f3f09b4
Matthieu Vagnon <main.mvagnon@icloud.com>
2025-04-03 23:06:03 +0900

    Typo.

--- src/components/see-more.tsx
+++ src/components/see-more.tsx
@@ -5,7 +5,7 @@ export default function SeeMore({ links }: { links: { name: string; url: string 
   return (
     <React.Fragment>
       <div className='flex flex-col items-center justify-center gap-2 mt-10 sm:mt-12 md:mt-14'>
-        <h3 className='font-sans-special text-lg sm:text-xl font-semibold'>More Informations on</h3>
+        <h3 className='font-sans-special text-lg sm:text-xl font-semibold'>Related Links</h3>
         <div className='flex flex-row gap-2'>
           {links.map((link) => (
             <LinkButton key={link.name} external variant='outline' href={link.url}>
