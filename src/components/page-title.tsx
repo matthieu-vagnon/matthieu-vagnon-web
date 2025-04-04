5611a3da42b17b732c22d2200656af26a16640be
Matthieu Vagnon <main.mvagnon@icloud.com>
2025-04-04 22:58:11 +0900

    Removing tailwind settings on h1.

--- src/components/page-title.tsx
+++ src/components/page-title.tsx
@@ -11,7 +11,7 @@ export default function PageTitle({
 }) {
   return (
     <div className={cn('flex flex-col items-center justify-center', className)}>
-      <h1 className='text-center'>{title}</h1>
+      <h1 className='text-center text-2xl sm:text-3xl md:text-4xl font-bold font-sans-special'>{title}</h1>
       <h2 className='text-base md:text-lg font-sans-special text-center mt-2 text-gray-500'>{description}</h2>
     </div>
   )
