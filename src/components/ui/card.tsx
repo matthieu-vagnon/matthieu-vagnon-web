73e81c8ac36027cb27c6500994302bf2e0b945a7
Matthieu Vagnon <main.mvagnon@icloud.com>
2025-04-12 00:16:44 +0900

    Bugfixes.

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -183,10 +183,10 @@ const Card = React.forwardRef<HTMLDivElement, CardProps>(
           <div className='relative w-full border-x border-zinc-400 dark:border-zinc-700'>
             <div className='absolute z-0 grid h-full w-full items-center'>
               <section className='absolute z-0 grid h-full w-full grid-cols-2 place-content-between'>
-                <div className='rounded-full outline outline-8 dark:outline-gray-950 sm:my-6 md:my-8 size-1 my-4 outline-gray-50 bg-[#007fff] -translate-x-[2.5px]' />
-                <div className='rounded-full outline outline-8 dark:outline-gray-950 sm:my-6 md:my-8 size-1 my-4 outline-gray-50 bg-[#007fff] translate-x-[2.5px] place-self-end' />
-                <div className='rounded-full outline outline-8 dark:outline-gray-950 sm:my-6 md:my-8 size-1 my-4 outline-gray-50 bg-[#007fff] -translate-x-[2.5px]' />
-                <div className='rounded-full outline outline-8 dark:outline-gray-950 sm:my-6 md:my-8 size-1 my-4 outline-gray-50 bg-[#007fff] translate-x-[2.5px] place-self-end' />
+                <div className='rounded-full outline-8 dark:outline-gray-950 sm:my-6 md:my-8 size-1 my-4 outline-gray-50 bg-[#007fff] -translate-x-[2.5px]' />
+                <div className='rounded-full outline-8 dark:outline-gray-950 sm:my-6 md:my-8 size-1 my-4 outline-gray-50 bg-[#007fff] translate-x-[2.5px] place-self-end' />
+                <div className='rounded-full outline-8 dark:outline-gray-950 sm:my-6 md:my-8 size-1 my-4 outline-gray-50 bg-[#007fff] -translate-x-[2.5px]' />
+                <div className='rounded-full outline-8 dark:outline-gray-950 sm:my-6 md:my-8 size-1 my-4 outline-gray-50 bg-[#007fff] translate-x-[2.5px] place-self-end' />
               </section>
             </div>
             <div className='relative z-20 mx-auto py-8'>{content}</div>
