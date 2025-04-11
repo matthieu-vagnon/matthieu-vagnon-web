3b6d9de7daaecf1e6d88467576c5750152a71d7f
Matthieu Vagnon <main.mvagnon@icloud.com>
2025-04-11 12:13:00 +0900

    Cookies expiring after the session -> Cookies expiring after 1 year.

--- src/lib/server-utils.ts
+++ src/lib/server-utils.ts
@@ -11,5 +11,9 @@ export async function getCookie(name: string): Promise<string | undefined> {
 export async function setCookie(name: string, value: string) {
   const cookieStore = await cookies()
 
-  cookieStore.set(name, value, { path: '/', httpOnly: false })
+  cookieStore.set(name, value, {
+    path: '/',
+    httpOnly: false,
+    expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 365)
+  })
 }
