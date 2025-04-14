18d6e13dc580b709fc71297ec3f128d3ae3730b6
Matthieu Vagnon <main.mvagnon@icloud.com>
2025-04-14 16:58:54 +0900

    Testimonials in english only.

--- data/testimonials.tsx
+++ data/testimonials.tsx
@@ -17,16 +17,6 @@ export const testimonials: Testimonial[] = [
           <Highlight>delivers on time</Highlight>, taking into account our requirements. I would
           <Highlight>highly recommend him</Highlight> as a service provider for any front application needed.
         </p>
-      ),
-      fr: (
-        <p>
-          Matthieu a fourni Predictive Layer avec un <Highlight>niveau de qualité élevé</Highlight> de produit
-          front-end. Il a développé <Highlight>plusieurs produits</Highlight> utilisés en direct par nos clients pour
-          consommer les prévisions produites par notre entreprise. Il est <Highlight>sérieux</Highlight> et toujours
-          <Highlight>livre à temps</Highlight>, en tenant compte de nos exigences. Je le
-          <Highlight>recommande</Highlight> vivement comme prestataire de service pour toutes les applications front
-          nécessaires.
-        </p>
       )
     },
     method: 'LinkedIn',
@@ -46,17 +36,6 @@ export const testimonials: Testimonial[] = [
           as the ability to <Highlight>adapt rapidly</Highlight> to our tools and processes. Matthieu has also
           demonstrated a real talent for developing <Highlight>ergonomic</Highlight> and{' '}
           <Highlight>performant</Highlight> user interfaces.
-        </p>
-      ),
-      fr: (
-        <p>
-          Matthieu a distingué par son <Highlight>expertise technique</Highlight>, sa <Highlight>créativité</Highlight>{' '}
-          et son <Highlight>engagement</Highlight> pour le succès des projets confiés à lui. Il a notamment démontré un
-          <Highlight>maîtrise parfaite</Highlight> des technologies ainsi que la capacité à{' '}
-          <Highlight>s&apos;adapter rapidement</Highlight> à nos outils et processus. Matthieu a également démontré un
-          talent réel pour le développement de <Highlight>ergonomie</Highlight> et de{' '}
-          <Highlight>performances</Highlight>
-          pour les utilisateurs.
         </p>
       )
     },
