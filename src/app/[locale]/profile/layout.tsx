23744526782425088e4771905cf938f63d5c434b
Matthieu Vagnon <main.mvagnon@icloud.com>
2025-04-20 00:01:16 +0900

    Optimizing SEO for newly created `profile` page.

---
+++ src/app/[locale]/profile/layout.tsx
@@ -0,0 +1,34 @@
+import { Metadata } from 'next';
+import { getMessages, getTranslations } from 'next-intl/server';
+
+export async function generateMetadata(): Promise<Metadata> {
+  const t = await getTranslations('profile.metadata');
+  const messages = (await getMessages()).profile.metadata;
+
+  return {
+    title: t('title'),
+    description: t('description'),
+    openGraph: {
+      type: 'website',
+      siteName: 'Matthieu Vagnon Web',
+      title: t('openGraph.title'),
+      description: t('openGraph.description'),
+      images: [
+        {
+          url: `${process.env.NEXT_PUBLIC_URL!}/og-image.png`,
+        },
+      ],
+    },
+    keywords: Object.keys(messages.keywords).map(
+      (key) => messages.keywords[key]
+    ),
+  };
+}
+
+export default function ProfileLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return children;
+}
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,33 +1,40 @@
-import { projects } from '@/data/projects'
-import { getPathname } from '@/i18n/navigation'
-import { routing } from '@/i18n/routing'
-import type { MetadataRoute } from 'next'
-import { Locale } from 'next-intl'
+import { projects } from '@/data/projects';
+import { getPathname } from '@/i18n/navigation';
+import { routing } from '@/i18n/routing';
+import type { MetadataRoute } from 'next';
+import { Locale } from 'next-intl';
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     ...getEntries('/'),
+    ...getEntries('/profile'),
     ...getEntries('/case-studies'),
-    ...Object.entries(projects).flatMap(([project, data]) => getEntries(`/case-studies/${project}`, data))
-  ]
+    ...Object.entries(projects).flatMap(([project, data]) =>
+      getEntries(`/case-studies/${project}`, data)
+    ),
+  ];
 }
 
-type Href = Parameters<typeof getPathname>[0]['href']
+type Href = Parameters<typeof getPathname>[0]['href'];
 
 function getEntries(href: Href, data?: Project) {
   return routing.locales.map((locale: Locale) => ({
     url: getUrl(href, locale),
     lastModified: new Date().toISOString(),
     changeFrequency: 'daily' as const,
-    images: data?.gallery?.img?.map((media) => `${process.env.NEXT_PUBLIC_URL!}${media.image.src}`),
+    images: data?.gallery?.img?.map(
+      (media) => `${process.env.NEXT_PUBLIC_URL!}${media.image.src}`
+    ),
     alternates: {
-      languages: Object.fromEntries(routing.locales.map((cur: Locale) => [cur, getUrl(href, cur)]))
-    }
-  }))
+      languages: Object.fromEntries(
+        routing.locales.map((cur: Locale) => [cur, getUrl(href, cur)])
+      ),
+    },
+  }));
 }
 
 function getUrl(href: Href, locale: Locale) {
-  const pathname = getPathname({ locale, href })
+  const pathname = getPathname({ locale, href });
 
-  return process.env.NEXT_PUBLIC_URL! + pathname
+  return process.env.NEXT_PUBLIC_URL! + pathname;
 }
