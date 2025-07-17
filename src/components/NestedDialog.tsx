d9c6b8c6e325b3a145bdb6f9736cc0b5731d4670
Matthieu Vagnon <main.mvagnon@icloud.com>
2025-07-17 14:49:41 +0900

    style: Enhance focus styles in globals.css and clean up DialogClose component in NestedDialog.tsx.

--- src/components/NestedDialog.tsx
+++ src/components/NestedDialog.tsx
@@ -85,7 +85,7 @@ const DialogContent = React.forwardRef<
         {...props}
       >
         {children}
-        <DialogClose className='hover:cursor-pointer absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground'>
+        <DialogClose className='hover:cursor-pointer absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground'>
           <X className='h-4 w-4' />
           <span className='sr-only'>Close</span>
         </DialogClose>
