9069097efc9c9807c75fd308876d5b7837eba114
Matthieu V <mvagnon@icloud.com>
2025-08-18 23:16:12 +0900

    ui: Minor UI on configuration modal with color preview and default theme.

--- src/components/ConfigurationModal.tsx
+++ src/components/ConfigurationModal.tsx
@@ -1,29 +1,29 @@
-'use client';
+"use client";
 
-import { useAccentColor } from '@/hooks/useAccentColor';
-import { useMagneticStatus } from '@/hooks/useMagneticStatus';
-import { useMessages, useTranslations } from 'next-intl';
-import { Button } from './Button';
-import { Checkbox } from './Checkbox';
-import { Label } from './Label';
+import { useAccentColor } from "@/hooks/useAccentColor";
+import { useMagneticStatus } from "@/hooks/useMagneticStatus";
+import { useMessages, useTranslations } from "next-intl";
+import { Button } from "./Button";
+import { Checkbox } from "./Checkbox";
+import { Label } from "./Label";
 import {
   DialogContent,
   DialogDescription,
   DialogHeader,
   DialogTitle,
-} from './NestedDialog';
+} from "./NestedDialog";
 import {
   Select,
   SelectContent,
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from './Select';
+} from "./Select";
 
 export function ConfigurationModal() {
   const { isMagnetic, changeIsMagnetic } = useMagneticStatus();
   const { accentColor, changeAccentColor } = useAccentColor();
-  const t = useTranslations('configure');
+  const t = useTranslations("configure");
   const messages = useMessages();
 
   const handleResetParameters = () => {
@@ -32,48 +32,59 @@ export function ConfigurationModal() {
   };
 
   return (
-    <DialogContent className='p-0'>
-      <DialogHeader className='border-b p-4'>
-        <DialogTitle>{t('title')}</DialogTitle>
-        <DialogDescription>{t('description')}</DialogDescription>
+    <DialogContent className="p-0">
+      <DialogHeader className="border-b p-4">
+        <DialogTitle>{t("title")}</DialogTitle>
+        <DialogDescription>{t("description")}</DialogDescription>
       </DialogHeader>
-      <div className='flex flex-col gap-4 pl-4 pr-4 pb-4 pt-1'>
-        <div className='flex flex-col gap-2'>
-          <Label>{t('accentColor.title')}</Label>
+      <div className="flex flex-col gap-4 pl-4 pr-4 pb-4 pt-1">
+        <div className="flex flex-col gap-2">
+          <Label>{t("accentColor.title")}</Label>
           <Select value={accentColor} onValueChange={changeAccentColor}>
             <SelectTrigger>
-              <SelectValue placeholder={t('accentColor.placeholder')} />
+              <SelectValue placeholder={t("accentColor.placeholder")} />
             </SelectTrigger>
             <SelectContent>
               {Object.entries(messages.configure.accentColor.colors).map(
                 ([key, value]) => (
                   <SelectItem key={key} value={key}>
-                    {value ? value.toString() : ''}
+                    <div className="flex gap-x-1">
+                      {value ? value.toString() : ""}
+                      {key === "default" && (
+                        <span className="text-xs text-muted-foreground self-baseline-last leading-4.5">
+                          {t("default")}
+                        </span>
+                      )}
+                    </div>
+                    <div
+                      className="h-2 w-2 rounded-full"
+                      style={{ backgroundColor: `var(--${key}-theme)` }}
+                    />
                   </SelectItem>
                 )
               )}
             </SelectContent>
           </Select>
         </div>
-        <div className='flex flex-col gap-2'>
-          <div className='flex flex-col gap-1 justify-between'>
-            <Label>{t('magnetic.title')}</Label>
-            <span className='text-xs text-muted-foreground'>
-              {t('magnetic.tip')}
+        <div className="flex flex-col gap-2">
+          <div className="flex flex-col gap-1 justify-between">
+            <Label>{t("magnetic.title")}</Label>
+            <span className="text-xs text-muted-foreground">
+              {t("magnetic.tip")}
             </span>
           </div>
           <Checkbox
             checked={isMagnetic}
             onCheckedChange={(checked) =>
               changeIsMagnetic(
-                checked === 'indeterminate' ? undefined : checked
+                checked === "indeterminate" ? undefined : checked
               )
             }
           />
         </div>
-        <div className='flex flex-row flex-wrap justify-end gap-2'>
-          <Button variant='ghost' noMagnetic onClick={handleResetParameters}>
-            {t('reset')}
+        <div className="flex flex-row flex-wrap justify-end gap-2">
+          <Button variant="ghost" noMagnetic onClick={handleResetParameters}>
+            {t("reset")}
           </Button>
         </div>
       </div>
