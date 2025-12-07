b40dedba44390c0b4b31780a2e15582e91fe30c3
Matthieu Vagnon <main.mvagnon@icloud.com>
2025-12-07 21:48:02 +0900

    fix: Dock on small devices

--- src/components/Dock.tsx
+++ src/components/Dock.tsx
@@ -138,7 +138,7 @@ function Dock({
           mouseX.set(Infinity);
         }}
         className={cn(
-          "mx-auto flex w-fit gap-1 sm:gap-4 rounded-2xl bg-accent px-4",
+          "mx-auto flex flex-row flex-nowrap w-fit gap-x-2 sm:gap-4 rounded-2xl bg-accent px-3 sm:px-4",
           className
         )}
         style={{ height: panelHeight }}
