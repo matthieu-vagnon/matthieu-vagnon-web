86c134fc74a745c03d050a846c98211a8d5043a2
Matthieu Vagnon <main.mvagnon@icloud.com>
2025-12-14 23:42:28 +0900

    ui: Enhance InfoBand component with new icons and URLs; improve animation and layout for better user experience

--- data/infoBand.tsx
+++ data/infoBand.tsx
@@ -1,4 +1,4 @@
-import { BadgeCheck } from "lucide-react";
+import { BadgeCheck, BadgeEuro } from "lucide-react";
 
 export const infoBand: InfoBand[] = [
   {
@@ -7,12 +7,15 @@ export const infoBand: InfoBand[] = [
       fr: "Membre certifié de la CCIFJ depuis juin 2025.",
     },
     icon: BadgeCheck,
+    url: {
+      en: "https://www.ccifj.or.jp/",
+    },
   },
   {
     text: {
-      en: "Certified member of the CCIFJ since June 2025.",
-      fr: "Test haha",
+      en: "Accredited for the French Innovation Tax Credit since 2025.",
+      fr: "Prestataire agréé CII (Crédit Impôt Innovation).",
     },
-    icon: BadgeCheck,
+    icon: BadgeEuro,
   },
 ];
--- src/components/InfoBand.tsx
+++ src/components/InfoBand.tsx
@@ -2,35 +2,102 @@
 
 import { infoBand } from "@/data/infoBand";
 import { cn, getTranslatedData } from "@/lib/utils";
+import { AnimatePresence, motion } from "framer-motion"; // Import Framer Motion
 import { useLocale } from "next-intl";
 import { useState } from "react";
-import { BlurFade } from "./BlurFade";
+import { LinkButton } from "./Button";
+import { ProgressiveBlur } from "./ProgressiveBlur";
+
+const MotionLinkButton = motion(LinkButton);
+
+const DURATION = 5000;
 
 export default function InfoBand({ className }: { className?: string }) {
   const [item, setItem] = useState(0);
   const locale = useLocale();
 
+  const infoBands = infoBand.length;
   const infoBandItem = infoBand[item];
-  const InfoBandIcon = infoBandItem.icon;
-  const infoBandText = infoBandItem.text;
+  const infoBandUrl = getTranslatedData(infoBandItem?.url, locale);
+  const InfoBandIcon = infoBandItem?.icon;
+  const infoBandText = infoBandItem?.text;
+
+  const handleNextItem = () => {
+    setItem((prev) => {
+      if (prev === infoBands - 1) return 0;
+      else return prev + 1;
+    });
+  };
 
   if (infoBand.length < 1) return null;
 
   return (
-    <BlurFade className="w-full pt-5 sm:pt-8">
-      <div
-        className={cn(
-          "py-2 px-3 flex gap-x-1.5 items-baseline rounded-lg w-full bg-main",
-          className
-        )}
-      >
-        <span className="text-center flex-1 text-main-foreground font-medium text-sm sm:text-lg">
-          {InfoBandIcon && (
-            <InfoBandIcon className="inline-flex size-3 sm:size-4 mr-1 sm:mr-1.5 mb-0.5" />
+    <>
+      <div className="h-16" />
+      <ProgressiveBlur
+        blurIntensity={0.33}
+        direction="top"
+        className="fixed top-0 left-0 pointer-events-none h-32 w-full z-1"
+      />
+      <div className="fixed top-4 left-0 px-4 z-99">
+        <style jsx global>{`
+          @keyframes progress-loading {
+            from {
+              width: 0%;
+            }
+            to {
+              width: 100%;
+            }
+          }
+        `}</style>
+        <MotionLinkButton
+          href={infoBandUrl ? infoBandUrl : "#"}
+          noMagnetic={!infoBandUrl}
+          noExternalIndicator={!infoBandUrl}
+          external={!!infoBandUrl}
+          size="lg"
+          layout
+          transition={{
+            type: "spring",
+            damping: 30,
+            stiffness: 400,
+          }}
+          className={cn(
+            "px-3! transition-none! relative group overflow-hidden cursor-default! flex gap-x-1.5 bg-main items-center text-main-foreground font-medium text-sm sm:text-lg no-underline! opacity-100!",
+            className
           )}
-          {getTranslatedData(infoBandText, locale)}
-        </span>
+        >
+          <div className="relative z-10 flex items-center gap-x-1.5">
+            <AnimatePresence mode="popLayout" initial={false}>
+              <motion.div
+                key={item}
+                layout="position"
+                initial={{ opacity: 0, filter: "blur(3px)" }}
+                animate={{ opacity: 1, filter: "blur(0px)" }}
+                exit={{ opacity: 0, filter: "blur(3px)" }}
+                transition={{
+                  duration: 0.33,
+                  ease: "easeInOut",
+                }}
+                className="flex items-center gap-x-2 origin-left"
+              >
+                {InfoBandIcon && (
+                  <InfoBandIcon className="inline-flex size-3 sm:size-4 mb-0.5" />
+                )}
+                {getTranslatedData(infoBandText, locale)}
+              </motion.div>
+            </AnimatePresence>
+          </div>
+          <div
+            key={item}
+            className="absolute bottom-0 left-0 h-1 bg-main-dark group-hover:paused!"
+            onAnimationEnd={handleNextItem}
+            style={{
+              animation: `progress-loading ${DURATION}ms linear forwards`,
+            }}
+          />
+        </MotionLinkButton>
       </div>
-    </BlurFade>
+    </>
   );
 }
