49cd72c2e39a6e156aec13fcde09e278f55fee2a
Matthieu Vagnon <main.mvagnon@icloud.com>
2025-12-19 19:05:02 +0900

    refactor: Remove special cover image and update case studies to include new cover image

--- src/app/[locale]/case-studies/page.tsx
+++ src/app/[locale]/case-studies/page.tsx
@@ -10,15 +10,22 @@ import cover1 from "@/public/project-card-cover/1.png";
 import cover2 from "@/public/project-card-cover/2.png";
 import cover3 from "@/public/project-card-cover/3.png";
 import cover4 from "@/public/project-card-cover/4.png";
-import coverSpecial from "@/public/project-card-cover/special.png";
+import cover5 from "@/public/project-card-cover/5.png";
 import { useTranslations } from "next-intl";
 import { StaticImageData } from "next/image";
 
-const COVER_IMAGES: StaticImageData[] = [cover1, cover2, cover3, cover4];
+const COVER_IMAGES: StaticImageData[] = [
+  cover1,
+  cover2,
+  cover3,
+  cover4,
+  cover5,
+];
 
 export default function CaseStudies() {
   const t = useTranslations();
   let blurDelay = 0;
+  let coverIndex = 0;
 
   return (
     <TestimonialsStatusWrapper shouldCollapse={true}>
@@ -32,17 +39,20 @@ export default function CaseStudies() {
         />
       </BlurFade>
       <div className="flex flex-row flex-wrap justify-center gap-2 sm:gap-4">
-        {Object.entries(projects).map(([key, project], index) => (
+        {Object.entries(projects).map(([key, project]) => (
           <BlurFade key={key} delay={blurDelay++ / 10}>
             <ProjectCard
-              coverImage={COVER_IMAGES[index % 4]}
+              coverImage={COVER_IMAGES[coverIndex++ % COVER_IMAGES.length]}
               url={`/case-studies/${key}`}
               project={project}
             />
           </BlurFade>
         ))}
         <BlurFade delay={blurDelay++ / 10}>
-          <ProjectCard coverImage={coverSpecial} seeMore />
+          <ProjectCard
+            coverImage={COVER_IMAGES[coverIndex++ % COVER_IMAGES.length]}
+            seeMore
+          />
         </BlurFade>
       </div>
       <BlurFade delay={blurDelay++ / 10}>
